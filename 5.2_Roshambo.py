021da03170d727b04ffd3376e7dad5ba9d34ec13
hueybry <hueybry@urbandaleschools.com>
2021-10-09 11:34:41 -0500

    Roshambo

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -10,7 +10,57 @@ Add conditional statements to figure out who wins and keep the records
 When the user quits print a win/loss record
 
 '''
+#1 = rock
+#2 = paper
+#3 = sissors
+hum = 0
+import random
+comp = 0
+q = False
+ply = 0
+while not q:
+    print("Do you want to play rock paper scissors?")
+    ply = input("Ans- ")
+    if ply.lower() == "yes" or ply.lower() == "ye" or ply.lower() == "y" or ply.lower == "sure":
+        for i in range(0,3):
+            comp = random.randint(1,3)# Inational processing before computing
+            print("Rock, Paper, or Scissors?")
+            hum = input("Ans- ")
+            if hum.lower() == "rock" or hum.lower() == "r":
+                hum = 1
+            elif hum.lower() == "paper" or hum.lower() == "p":
+                hum = 2
+            elif hum.lower() == "scissors" or hum.lower == "scissor" or hum.lower == "s":
+                hum = 3
+            else:
+                print("Please enter Rock Paper or Scissors")
+                print("Try again")
+                break
+            if comp == 1:
+                print("Comp- Rock")
+            elif comp == 2:
+                print("Comp- Paper")
+            else:
+                print("Comp- Scissors")
 
+            if hum == 1 and comp == 2: # Logic system
+                print("You lost")
+            elif hum == 1 and comp == 3:
+                print("You Won")
+            elif hum == 2 and comp == 1:
+                print("You Won")
+            elif hum == 2 and comp == 3:
+                print("You Lost")
+            elif hum == 3 and comp == 2:
+                print("You Won")
+            elif hum == 3 and comp == 1:
+                print("You lost")
+            else:
+                print("Your tided")
+    elif ply.lower() == "no" or ply.lower() == "n" or ply.lower() == "nope":
+        break
+    else:
+        print("Please answer with yes or no")
 
 
 
