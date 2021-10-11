7d188daa4eb2eea355be797e199cab4a965e10c7
hueybry <hueybry@urbandaleschools.com>
2021-10-11 10:10:14 -0500

    Camel Game 1.1

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -26,28 +26,40 @@ while not q:
     print("Do you want to play rock paper scissors?")
     ply = input("Ans- ")
     if ply.lower() == "yes" or ply.lower() == "yeah" or ply.lower() == "y" or ply.lower() == "sure":
-        for i in range(0,3):
+        print("Press s for scissors,")
+        print("Press r for rock,")
+        print("and Press p for paper")
+        print("You can quit at anytime by pressing q")
+        while not q:
             comp = random.randint(1,3)# Inational processing before computing------------------
             print("Rock, Paper, or Scissors?")
             hum = input("Ans- ")
-            games += 1
+
             if hum.lower() == "rock" or hum.lower() == "r":
                 hum = 1
             elif hum.lower() == "paper" or hum.lower() == "p":
                 hum = 2
             elif hum.lower() == "scissors" or hum.lower() == "scissor" or hum.lower() == "s":
                 hum = 3
+            elif hum.lower() == "q" or hum.lower() == "quit" or hum.lower() == "leave" or hum.lower() == "l":
+                q = True
+                break
             else:
                 print("Please enter Rock Paper or Scissors")
                 print("Try again")
                 i -= 1
                 continue
+            games += 1
+
+
             if comp == 1:
                 print("Comp- Rock")
             elif comp == 2:
                 print("Comp- Paper")
-            else:
+            elif comp == 3:
                 print("Comp- Scissors")
+            else:
+                pass
 
             if hum == 1 and comp == 2: # Logic system------------------------------
                 print("You lost")
@@ -68,8 +80,11 @@ while not q:
                 print("You lost")
                 lose +=1
             else:
-                print("Your tied")
-                tie += 1            #End of Logic unit -------------------------------------
+                if not q:
+                    print("Your tied")
+                    tie += 1
+                else:
+                    pass                #End of Logic unit -------------------------------------
 
     elif ply.lower() == "no" or ply.lower() == "n" or ply.lower() == "nope":
         break
--- 5.3_Camel_Game.py
+++ 5.3_Camel_Game.py
@@ -8,12 +8,18 @@ import random
 import time
 # Varibles---------------------------------
 # 1 wolves
+wolv = 0
 # 2 hunters
+hunt = 0
 # 3 paranoid
-shas = 0
+para = 0
+shas = 0 #gamemode
+
+miles = 0
 done = False
 dead = False
 hum = 0
+day = 0
 print("Welcome to the camel game!")
 print("Your goal is to travel 200 miles though deep forests")
 
@@ -32,25 +38,33 @@ while not done:
         print("F. Divert from the main path (slow)")
         print("Q. Quit.")
         hum = input("- ")
-        if hum.lower() == "a":
+        if hum.lower() == "a": #Drink
+            day +=1
+
+        elif hum.lower() == "b": #Mod Sped
             continue
-        elif hum.lower() == "b":
+        elif hum.lower() == "c": #Full Sped
+            miles +=
+        elif hum.lower() == "d": #Stop night
             continue
-        elif hum.lower() == "c":
+        elif hum.lower() == "e": #Status check
             continue
-        elif hum.lower() == "d":
+        elif hum.lower() == "f": #Divert
             continue
-        elif hum.lower() == "e":
-            continue
-        elif hum.lower() == "f":
-            continue
-        elif hum.lower() == "q":
+        elif hum.lower() == "q": #Quit
             print("You quit.")
             done = True
             break
         else:
             print("Please answer with a letter")
+        if shas == 1:
+            wolv += 8
+        elif shas == 2:
+            hunt += 13
+        else:
+            para += 10
 
+        if
 
 
 
